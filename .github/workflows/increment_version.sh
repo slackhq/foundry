7f69e0ce353661b89c9c946c319e2378c18d9845
Zac Sweers <pandanomic@gmail.com>
2022-04-19 01:37:36 -0400

    Fix increment_version.sh

--- .github/workflows/increment_version.sh
+++ .github/workflows/increment_version.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# Target project to update, required
-TARGET=$1
 # Version to increment to, optional. If blank or not specified, will auto-increment.
-REQUESTED_VERSION=$2
+REQUESTED_VERSION=$1
 source .github/workflows/scriptUtil.sh
 # Parse the current version, strip leading zeros, increment
-CURRENT_VERSION=$(getProperty 'VERSION_NAME' "$TARGET"/gradle.properties)
+CURRENT_VERSION=$(getProperty 'VERSION_NAME' gradle.properties)
 
-# Export the coordinates while we're at it for later use in publish.yml
+# Export the coordinates while we're at it for later use in publish-*.yml files
 # Group is always in our root dir
 GROUP=$(getProperty 'GROUP' gradle.properties)
-ARTIFACT=$(getProperty 'POM_ARTIFACT_ID' "$TARGET"/gradle.properties)
+ARTIFACT=$(getProperty 'POM_ARTIFACT_ID' gradle.properties)
 
 STRIPPED_CURRENT=$(echo "$CURRENT_VERSION" | sed 's/^0*//')
 if [[ "$REQUESTED_VERSION" != "" ]]; then
@@ -20,10 +18,8 @@ else
   ((STRIPPED_CURRENT++))
   NEW_VERSION=$(printf "%05d\n" $STRIPPED_CURRENT)
 fi
-sed -i -e "s/${CURRENT_VERSION}/${NEW_VERSION}/g" "$TARGET"/gradle.properties
+sed -i -e "s/${CURRENT_VERSION}/${NEW_VERSION}/g" gradle.properties
 echo "current: $CURRENT_VERSION"
 echo "CURRENT_VERSION=$CURRENT_VERSION" >> $GITHUB_ENV
 echo "new: $NEW_VERSION"
 echo "NEW_VERSION=$NEW_VERSION" >> $GITHUB_ENV
-# We just use the artifact ID in the android repo for the coordinate
-echo "COORDINATES=$ARTIFACT" >> $GITHUB_ENV
