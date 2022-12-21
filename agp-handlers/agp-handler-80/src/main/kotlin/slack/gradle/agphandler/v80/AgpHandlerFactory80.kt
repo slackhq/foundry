72a7e49c10da50f4728d4f8644912743d3458265
Zac Sweers <pandanomic@gmail.com>
2022-12-21 18:36:08 -0500

    Further generify packagingOptions backporting for 8.x

--- agp-handlers/agp-handler-80/src/main/kotlin/slack/gradle/agphandler/v80/AgpHandlerFactory80.kt
+++ agp-handlers/agp-handler-80/src/main/kotlin/slack/gradle/agphandler/v80/AgpHandlerFactory80.kt
@@ -15,7 +15,7 @@
  */
 package slack.gradle.agphandler.v80
 
-import com.android.build.api.dsl.PackagingOptions
+import com.android.build.api.dsl.CommonExtension
 import com.android.build.gradle.internal.dsl.TestOptions
 import com.android.builder.model.Version.ANDROID_GRADLE_PLUGIN_VERSION
 import com.google.auto.service.AutoService
@@ -43,10 +43,14 @@ private class AgpHandler80 : AgpHandler {
     options.all(body)
   }
 
-  override fun jniLibsPickFirst(
-    packagingOptions: PackagingOptions,
-    pickFirsts: Collection<String>
+  override fun packagingOptions(
+    commonExtension: CommonExtension<*, *, *, *>,
+    resourceExclusions: Collection<String>,
+    jniPickFirsts: Collection<String>
   ) {
-    packagingOptions.jniLibs.pickFirsts += pickFirsts
+    commonExtension.packagingOptions {
+      resources.excludes += resourceExclusions
+      jniLibs.pickFirsts += jniPickFirsts
+    }
   }
 }
