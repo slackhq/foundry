812be50e28c8cd2e918c9d75cdbf043a43254540
Zac Sweers <pandanomic@gmail.com>
2023-04-14 14:05:19 +0200

    Introduce GitExecValueSource
    This replaces a bunch of places we were manually doing process execs and not being compatible with configuration cache. This is required for Gradle 8.0

---
+++ slack-plugin/src/main/kotlin/slack/gradle/util/GitExecValueSource.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package slack.gradle.util
+
+import java.io.ByteArrayOutputStream
+import java.nio.charset.Charset
+import javax.inject.Inject
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.ProviderFactory
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+import org.gradle.process.ExecOperations
+
+internal fun ProviderFactory.gitVersionProvider(): Provider<String> {
+  return gitExecProvider("git", "--version")
+}
+
+internal fun ProviderFactory.gitExecProvider(vararg args: String): Provider<String> {
+  return of(GitExecValueSource::class.java) { parameters.args.addAll(*args) }
+}
+
+// Adapted from
+// https://docs.gradle.org/8.1/userguide/configuration_cache.html#config_cache:requirements:external_processes
+internal abstract class GitExecValueSource : ValueSource<String, GitExecValueSource.Parameters> {
+  @get:Inject abstract val execOperations: ExecOperations
+
+  override fun obtain(): String {
+    val output = ByteArrayOutputStream()
+    execOperations.exec {
+      commandLine(parameters.args.get())
+      standardOutput = output
+    }
+    return String(output.toByteArray(), Charset.defaultCharset()).trim { it <= ' ' }
+  }
+
+  interface Parameters : ValueSourceParameters {
+    val args: ListProperty<String>
+  }
+}
