cffb120e80ebc69446433cc5adeda35037449658
Katherine Liu <liu.k@slack-corp.com>
2023-06-15 17:28:27 -0700

    Added java sourceCompatibility for Java Version 20

--- skate-plugin/build.gradle.kts
+++ skate-plugin/build.gradle.kts
@@ -9,6 +9,10 @@ group = "com.slack.sgp.intellij"
 version = "1.0-SNAPSHOT"
 
 repositories { mavenCentral() }
+java {
+    sourceCompatibility = JavaVersion.VERSION_20
+    targetCompatibility = JavaVersion.VERSION_20
+}
 
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
