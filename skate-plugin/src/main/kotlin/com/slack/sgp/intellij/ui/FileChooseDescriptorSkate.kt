f493fa6d8dd8e73230c5b135b74790f5b59b924e
Katherine Liu <liu.k@slack-corp.com>
2023-06-26 13:48:48 -0700

    run spotlessApply

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/ui/FileChooseDescriptorSkate.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/ui/FileChooseDescriptorSkate.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.slack.sgp.intellij.ui
 
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
@@ -11,6 +26,7 @@ object FileChooseDescriptorSkate {
   private fun createSingleFileChooserDescriptor(fileFilter: (VirtualFile) -> Boolean) =
     object : FileChooserDescriptor(true, false, false, false, false, false) {
       override fun isFileSelectable(file: VirtualFile?) = file?.let { fileFilter(it) } ?: false
+
       override fun isFileVisible(file: VirtualFile?, showHiddenFiles: Boolean) =
         if (file == null || file.isDirectory) true else fileFilter(file)
     }
