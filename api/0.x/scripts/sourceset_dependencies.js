d8a823b1ea173d8f8eb82619823c2d02a9e8c63b
Zac Sweers <pandanomic@gmail.com>
2023-06-27 13:33:58 -0400

    Deployed 8c2d00c with MkDocs version: 1.4.2

---
+++ api/0.x/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":slack-plugin:dokkaHtmlPartial/main":[],":agp-handlers:agp-handler-api:dokkaHtmlPartial/main":[],":sgp-monkeypatch-agp:dokkaHtmlPartial/main":[],":agp-handlers:agp-handler-80:dokkaHtmlPartial/generatedByKspKotlin":[],":agp-handlers:agp-handler-80:dokkaHtmlPartial/main":[]}'
