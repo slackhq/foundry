8a40e0afa366b277cd4b728c5bff6905e8c7a52a
Katherine Liu <67719108+kateliu20@users.noreply.github.com>
2023-06-27 10:44:34 -0700

    Creating a Settings for Skate so users can choose update file to display (#412)
    Created the UI component for Skate Plugin, to make the Skate Settings
    visible and allow users to change the file path to display on the
    "What's New" Panel. It also has a checkbox that when clicked, the
    "What's New" panel is visible, but unclicked it is not on the toolbar
    side panel.
    
    It also defaults to "CHANGELOG.md" unless user picks otherwise, and
    users can only select .md files
    
    <img width="1296" alt="Screenshot 2023-06-26 at 3 43 24 PM"
    src="https://github.com/slackhq/slack-gradle-plugin/assets/67719108/fe4afbf5-34dc-4c5e-88f9-c0daffbba13e">
    
    <!--
    
      ⬆ Put your description above this! ⬆
    
      Please be descriptive and detailed.
      
    Please read our [Contributing
    Guidelines](https://github.com/tinyspeck/slack-gradle-plugin/blob/main/.github/CONTRIBUTING.md)
    and [Code of Conduct](https://slackhq.github.io/code-of-conduct).
    
    Don't worry about deleting this, it's not visible in the PR!
    -->
    
    ---------
    
    Co-authored-by: Zac Sweers <pandanomic@gmail.com>

---
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/SkatePluginSettings.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.sgp.intellij
+
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.SimplePersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.openapi.project.Project
+
+/** Manages user-specific settings for the Skate plugin */
+@Service(Service.Level.PROJECT)
+@State(name = "SkatePluginSettings", storages = [Storage("skate.xml")])
+class SkatePluginSettings(
+  private val project: Project,
+) : SimplePersistentStateComponent<SkatePluginSettings.State>(State()) {
+
+  var whatsNewFilePath: String
+    get() = state.whatsNewFilePath ?: "CHANGELOG.md"
+    set(value) {
+      state.whatsNewFilePath = value
+    }
+
+  var isWhatsNewEnabled: Boolean
+    get() = state.isWhatsNewEnabled
+    set(value) {
+      state.isWhatsNewEnabled = value
+    }
+
+  class State : BaseState() {
+    var whatsNewFilePath by string()
+    var isWhatsNewEnabled by property(true)
+  }
+}
---
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/ui/SkateConfigUI.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.sgp.intellij.ui
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.DialogPanel
+import com.intellij.openapi.vfs.LocalFileSystem
+import com.intellij.ui.dsl.builder.Panel
+import com.intellij.ui.dsl.builder.bindSelected
+import com.intellij.ui.dsl.builder.bindText
+import com.intellij.ui.dsl.builder.panel
+import com.slack.sgp.intellij.SkateBundle
+import com.slack.sgp.intellij.SkatePluginSettings
+import java.io.File
+
+internal class SkateConfigUI(
+  private val settings: SkatePluginSettings,
+  private val project: Project,
+) {
+
+  fun createPanel(): DialogPanel = panel {
+    checkBoxRow()
+    choosePathRow()
+  }
+
+  private fun Panel.checkBoxRow() {
+    row(SkateBundle.message("skate.configuration.enableWhatsNew.title")) {
+      checkBox(SkateBundle.message("skate.configuration.enableWhatsNew.description"))
+        .bindSelected(
+          getter = { settings.isWhatsNewEnabled },
+          setter = { settings.isWhatsNewEnabled = it }
+        )
+    }
+  }
+
+  private fun Panel.choosePathRow() {
+    row(SkateBundle.message("skate.configuration.choosePath.title")) {
+      textFieldWithBrowseButton(
+          SkateBundle.message("skate.configuration.choosePath.dialog.title"),
+          project,
+          FileChoosing.singleMdFileChooserDescriptor()
+        )
+        .bindText(
+          getter = {
+            LocalFileSystem.getInstance().extractPresentableUrl(settings.whatsNewFilePath)
+          },
+          setter = {
+            if (File(it).isFile) {
+              settings.whatsNewFilePath =
+                LocalFileSystem.getInstance().findFileByPath(it)?.path.orEmpty()
+            } else {
+              settings.whatsNewFilePath = ""
+            }
+          }
+        )
+        .enabled(settings.isWhatsNewEnabled)
+    }
+  }
+}
