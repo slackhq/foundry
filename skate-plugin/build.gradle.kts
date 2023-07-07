728a44af5471a31284436bc848a9b95b7ef33b3d
Katherine Liu <67719108+kateliu20@users.noreply.github.com>
2023-07-06 22:30:14 -0700

    Update skate-plugin/build.gradle.kts to .add
    Co-authored-by: Zac Sweers <pandanomic@gmail.com>

--- skate-plugin/build.gradle.kts
+++ skate-plugin/build.gradle.kts
@@ -16,7 +16,7 @@ intellij {
   version.set("2022.2.5")
   type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf("org.intellij.plugins.markdown"))
+  plugins.add("org.intellij.plugins.markdown")
 }
 
 tasks {
