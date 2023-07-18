ed58373e8b3f225108330d0383d893470ed66fcb
Katherine Liu <liu.k@slack-corp.com>
2023-07-17 17:19:05 -0700

    Add initialization for skate config settings

--- skate-plugin/src/test/kotlin/com/slack/sgp/intellij/SkatePluginInitializationTest.kt
+++ skate-plugin/src/test/kotlin/com/slack/sgp/intellij/SkatePluginInitializationTest.kt
@@ -1,15 +1,29 @@
 package com.slack.sgp.intellij
 
+import com.google.common.truth.Truth.assertThat
 import com.intellij.openapi.components.service
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.junit.Test
 
 class SkatePluginInitializationTest : BasePlatformTestCase() {
 
+  @Test
   fun `test Skate Plugin Service Initialization to ensure SkateProjectService is properly registered & initialized`() {
     val skateService = project.service<SkateProjectService>()
-    assertTrue(
-      "Service should be instance of SkateProjectServiceImpl",
-      skateService is SkateProjectServiceImpl
-    )
+
+    // Service should be an instance of SkateProjectServiceImpl
+    assertThat(skateService).isInstanceOf(SkateProjectServiceImpl::class.java)
+  }
+
+  @Test
+  fun `test Skate Plugin Settings Initialization`() {
+    val settings = project.service<SkatePluginSettings>()
+
+    // Assert that settings is not null
+    assertThat(settings).isNotNull()
+
+    // Check the default values
+    assertThat(settings.whatsNewFilePath).isEqualTo("CHANGELOG.md")
+    assertThat(settings.isWhatsNewEnabled).isTrue()
   }
 }
