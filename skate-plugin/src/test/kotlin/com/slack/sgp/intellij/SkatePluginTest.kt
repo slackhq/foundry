2a378fec9db30e91d8b600a7adb4d7c8f02bee90
Katherine Liu <liu.k@slack-corp.com>
2023-07-18 17:12:58 -0700

    Add comments and account for tool Window

--- skate-plugin/src/test/kotlin/com/slack/sgp/intellij/SkatePluginTest.kt
+++ skate-plugin/src/test/kotlin/com/slack/sgp/intellij/SkatePluginTest.kt
@@ -1,6 +1,9 @@
 package com.slack.sgp.intellij
 
+import com.google.common.truth.Truth.assertThat
 import com.intellij.remoterobot.RemoteRobot
+import com.intellij.remoterobot.fixtures.ComponentFixture
+import com.intellij.remoterobot.search.locators.byXpath
 import org.junit.Test
 
 class SkatePluginTest {
@@ -9,5 +12,15 @@ class SkatePluginTest {
   fun checkToolWindow() {
     // Create a RemoteRobot instance with the default URL (localhost:8082)
     val robot = RemoteRobot("http://127.0.0.1:8082")
+
+    // See if Tool Window has the same name it's supposed to
+    val toolWindow =
+      robot.find(
+        ComponentFixture::class.java,
+        byXpath("//div[@accessibilityName=\"What's New in Slack!\"]")
+      )
+
+    // Check if the Tool Window is showing
+    assertThat(toolWindow.isShowing).isTrue()
   }
 }
