3fbc17c7becd1619bb2ac46044fa8fcb1ec4011f
Zac Sweers <pandanomic@gmail.com>
2023-07-21 17:40:20 -0400

    Implement artifactory authenticator plugin + artifactory publishing for IJ plugins (#479)
    This implements support for uploading intellij plugins to artifactory
    using gradle-property-gated values and a new (extremely small) plugin
    for supporting authorized access to artifactory.
    
    The new plugin is under `:skate-plugin:artifactory-authenticator` and
    just exposes a single setting to configure its values + hook into
    intellij's plugin repositories hook for authentication.
    
    This also cleans up some common IJ plugin configs since we now have
    multiple.
    
    <img width="1094" alt="Screenshot 2023-07-21 at 1 09 44 PM"
    src="https://github.com/slackhq/slack-gradle-plugin/assets/1361086/7442dd32-4cab-4bef-b1c5-792c3f11b31c">
    
    
    <!--
      ⬆ Put your description above this! ⬆
    
      Please be descriptive and detailed.
      
    Please read our [Contributing
    Guidelines](https://github.com/tinyspeck/slack-gradle-plugin/blob/main/.github/CONTRIBUTING.md)
    and [Code of Conduct](https://slackhq.github.io/code-of-conduct).
    
    Don't worry about deleting this, it's not visible in the PR!
    -->

---
+++ skate-plugin/artifactory-authenticator/src/main/kotlin/com/slack/intellij/artifactory/AuthPluginSettings.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.intellij.artifactory
+
+import com.intellij.ide.plugins.auth.PluginRepositoryAuthListener
+import com.intellij.openapi.components.BaseState
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.SimplePersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+
+@Service(Service.Level.APP)
+@State(name = "ArtifactoryAuthSettings", storages = [Storage("artifactoryAuthSettings.xml")])
+class AuthPluginSettings : SimplePersistentStateComponent<AuthPluginSettings.State>(State()) {
+
+  var enabled: Boolean
+    get() = state.enabled
+    set(value) {
+      val previous = state.enabled
+      state.enabled = value
+      notifyChange(previous != value)
+    }
+
+  var url: String?
+    get() = state.url
+    set(value) {
+      val previous = state.url
+      state.url = value
+      notifyChange(previous != value)
+    }
+
+  var username: String?
+    get() = state.username
+    set(value) {
+      val previous = state.username
+      state.username = value
+      notifyChange(previous != value)
+    }
+
+  var token: String?
+    get() = state.token
+    set(value) {
+      val previous = state.token
+      state.token = value
+      notifyChange(previous != value)
+    }
+
+  private fun notifyChange(changed: Boolean) {
+    if (changed) {
+      // Any time we change this setting we need to notify the IDE that the auth has changed
+      PluginRepositoryAuthListener.notifyAuthChanged()
+    }
+  }
+
+  class State : BaseState() {
+    var enabled by property(false)
+    var url by string()
+    var username by string()
+    var token by string()
+  }
+}
