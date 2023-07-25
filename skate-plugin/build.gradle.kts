79cf0cff1d7261f5a7d896a88cf26110e7f97457
Zac Sweers <pandanomic@gmail.com>
2023-07-25 16:00:55 -0400

    Add git sha to plugin publishing

--- skate-plugin/build.gradle.kts
+++ skate-plugin/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.jetbrains.plugin.structure.base.utils.exists
+import java.nio.file.Paths
+import java.util.Locale
+import kotlin.io.path.readText
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -20,6 +24,44 @@ intellij {
   plugins.add("org.intellij.plugins.markdown")
 }
 
+fun isGitHash(hash: String): Boolean {
+  if (hash.length != 40) {
+    return false
+  }
+
+  return hash.all { it in '0'..'9' || it in 'a'..'f' }
+}
+
+// Impl from https://gist.github.com/madisp/6d753bde19e278755ec2b69ccfc17114
+fun readGitRepoCommit(): String? {
+  try {
+    val head = Paths.get("${rootProject.projectDir}/.git").resolve("HEAD")
+    if (!head.exists()) {
+      return null
+    }
+
+    val headContents = head.readText(Charsets.UTF_8).lowercase(Locale.US).trim()
+
+    if (isGitHash(headContents)) {
+      return headContents
+    }
+
+    if (!headContents.startsWith("ref:")) {
+      return null
+    }
+
+    val headRef = headContents.removePrefix("ref:").trim()
+    val headFile = Paths.get(".git").resolve(headRef)
+    if (!headFile.exists()) {
+      return null
+    }
+
+    return headFile.readText(Charsets.UTF_8).trim().takeIf { isGitHash(it) }
+  } catch (e: Exception) {
+    return null
+  }
+}
+
 // region Version.kt template for setting the project version in the build
 sourceSets { main { java.srcDir("$buildDir/generated/sources/version-templates/kotlin/main") } }
 
@@ -32,7 +74,7 @@ val copyVersionTemplatesProvider =
       mapOf(
         "projectVersion" to project.property("VERSION_NAME").toString(),
         "bugsnagKey" to project.findProperty("SgpIntellijBugsnagKey")?.toString().orEmpty(),
-        "gitSha" to project.findProperty("GIT_SHA")?.toString().orEmpty(),
+        "gitSha" to readGitRepoCommit().orEmpty(),
       )
     )
     filteringCharset = "UTF-8"
