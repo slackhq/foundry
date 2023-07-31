3f2536b386ee924943dc38193c756320d4f9981f
Katherine Liu <liu.k@slack-corp.com>
2023-07-31 11:15:05 -0700

    Migrate to artifactory-authenticator

--- skate-plugin/artifactory-authenticator/src/main/kotlin/com/slack/intellij/artifactory/RepoAuthHotfix.kt
+++ skate-plugin/artifactory-authenticator/src/main/kotlin/com/slack/intellij/artifactory/RepoAuthHotfix.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.slack.sgp.intellij
+package com.slack.intellij.artifactory
 
 import com.intellij.ide.AppLifecycleListener
 import com.intellij.ide.plugins.auth.PluginRepositoryAuthService
