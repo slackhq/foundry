6ad68389ada060fe7c551e14b344b1103a5eae33
Katherine Liu <liu.k@slack-corp.com>
2023-07-31 10:28:45 -0700

    Add PluginRepositoryAuthService fix

---
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/RepoAuthHotfix.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.slack.sgp.intellij
+
+import com.intellij.ide.AppLifecycleListener
+import com.intellij.ide.plugins.auth.PluginRepositoryAuthService
+import com.intellij.openapi.components.service
+
+// code from:
+// https://youtrack.jetbrains.com/issue/IDEA-315487/PluginRepositoryAuthProvider-is-broken-in-2022.3-making-custom-repository-authentication-impossible#focus=Comments-27-6990965.0-0
+class RepoAuthHotfix : AppLifecycleListener {
+  override fun appFrameCreated(commandLineArgs: MutableList<String>) {
+    service<PluginRepositoryAuthService>()
+  }
+}
