a2661a7a0b3f904f8a4108fca7207879cb1e5ce9
Katherine Liu <liu.k@slack-corp.com>
2023-08-08 14:03:45 -0700

    Remove unnecessary code for relative path

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/SkatePluginSettings.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/SkatePluginSettings.kt
@@ -20,28 +20,16 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.SimplePersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
-import com.intellij.openapi.project.Project
-import java.nio.file.Paths
 
 /** Manages user-specific settings for the Skate plugin */
 @Service(Service.Level.PROJECT)
 @State(name = "SkatePluginSettings", storages = [Storage("skate.xml")])
-class SkatePluginSettings(private val project: Project) :
-  SimplePersistentStateComponent<SkatePluginSettings.State>(State()) {
+class SkatePluginSettings : SimplePersistentStateComponent<SkatePluginSettings.State>(State()) {
 
   var whatsNewFilePath: String
     get() = state.whatsNewFilePath ?: "WHATSNEW.md"
     set(value) {
-      val file = Paths.get(value)
-      val projectDir = Paths.get(project.basePath!!)
-
-      // If the file is in the project directory, just store the filename
-      if (file.startsWith(projectDir)) {
-        state.whatsNewFilePath = file.fileName.toString()
-      } else {
-        // Otherwise, store the full path
-        state.whatsNewFilePath = file.toAbsolutePath().toString()
-      }
+      state.whatsNewFilePath = value
     }
 
   var isWhatsNewEnabled: Boolean
