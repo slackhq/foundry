ff6a0a674c5db1b31ad7741d57ccbb6a64697bf8
Arpita Patel <apatel@slack-corp.com>
2023-09-07 15:05:35 -0400

    Clean up Test

--- skate-plugin/src/test/kotlin/com/slack/sgp/intellij/featureflags/FeatureFlagExtractorTest.kt
+++ skate-plugin/src/test/kotlin/com/slack/sgp/intellij/featureflags/FeatureFlagExtractorTest.kt
@@ -1,51 +1,40 @@
+/*
+ * Copyright (C) 2023 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.slack.sgp.intellij.featureflags
 
 import com.google.common.truth.Truth.assertThat
-import com.intellij.icons.AllIcons
-import com.intellij.openapi.components.ServiceManager
-import com.intellij.testFramework.TestIndexingModeSupporter
-import com.intellij.testFramework.builders.JavaModuleFixtureBuilder
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase
-import org.jetbrains.kotlin.psi.KtFile
-import org.junit.Test
-import org.jetbrains.kotlin.psi.KtPsiFactory
-import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.FileTypeManager
-
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.idea.KotlinLanguage
+import com.intellij.psi.PsiFileFactory
 import io.mockk.mockk
-import org.jetbrains.kotlin.idea.KotlinFileType
+import org.junit.Test
 
-class FeatureFlagExtractorTest {
+class FeatureFlagExtractorTest{
 
-  fun createPsiFromContent(project: Project, content: String): KtFile {
+  private fun createKtPsiFileFromContent(project: Project, content: String): PsiFile {
     val fileType = FileTypeManager.getInstance().getFileTypeByExtension("kt")
-
-//    val instance = PsiFileFactory.getInstance(project)
-//    if (instance !is PsiFileFactory) {
-//      throw IllegalStateException("Unexpected type: ${instance::class.java.name}")
-//    }
-    val fileFactory = project.getService(PsiFileFactory::class.java)
-
-
-
-//    val fileFactory: PsiFileFactory = PsiFileFactory.getInstance(project)
-    val psiFile: PsiFile = fileFactory.createFileFromText("Temporary.kt", fileType, content)
-
-//    if (psiFile !is KtFile) {
-//      throw IllegalArgumentException("Failed to create KtFile")
-//    }
-
-    return psiFile
+    val psiFactory = PsiFileFactory.getInstance(project)
+    return psiFactory.createFileFromText("Temporary.kt", fileType, content)
   }
 
   @Test
   fun testExtractFeatureFlags() {
-    val fileContent = """
+    val fileContent =
+      """
         @FeatureFlags(ForBlockKitFeature::class)
         enum class BlockKitFeature : FeatureFlagEnum {
 
@@ -73,15 +62,15 @@ class FeatureFlagExtractorTest {
         @Qualifier
         @Target(AnnotationTarget.VALUE_PARAMETER, AnnotationTarget.FUNCTION, AnnotationTarget.FIELD)
         annotation class ForBlockKitFeature(val feature: BlockKitFeature)
-        """.trimIndent()
+        """
+        .trimIndent()
 
     // Mock a Project instance
     val mockProject = mockk<Project>(relaxed = true)
-    // When: We create a PSI representation of the content and extract feature flags
-    val psiFile = createPsiFromContent(mockProject, fileContent)
+    // Create a PSI representation of the content and extract feature flags
+    val psiFile = createKtPsiFileFromContent(mockProject, fileContent)
     val extractor = FeatureFlagExtractor()
-    val featureFlags = extractor.extractFeatureFlags(psiFile as PsiFile)
-
+    val featureFlags = extractor.extractFeatureFlags(psiFile)
 
     // Verifying the correct extraction of feature flags.
     assertThat(featureFlags).contains("DATE_TIME_ELEMENT_BLOCK_CLIENT")
@@ -90,5 +79,4 @@ class FeatureFlagExtractorTest {
     assertThat(featureFlags).contains("RICH_TEXT_ELEMENT_BLOCK_CLIENT")
     assertThat(featureFlags).contains("ANDROID_INPUT_BOTTOM_SHEET_REVAMP")
   }
-
 }
