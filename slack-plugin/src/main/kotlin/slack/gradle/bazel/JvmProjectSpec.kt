9a67ac20d87046aaa875ccbc06a4ef23421db457
Zac Sweers <pandanomic@gmail.com>
2024-02-28 17:30:50 -0500

    Add test target support

--- slack-plugin/src/main/kotlin/slack/gradle/bazel/JvmProjectSpec.kt
+++ slack-plugin/src/main/kotlin/slack/gradle/bazel/JvmProjectSpec.kt
@@ -1,12 +1,29 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package slack.gradle.bazel
 
 import com.android.build.gradle.internal.publishing.AndroidArtifacts
+import java.util.SortedSet
 import okio.FileSystem
 import okio.Path
+import okio.Path.Companion.toOkioPath
 import org.gradle.api.DefaultTask
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.component.ModuleComponentIdentifier
+import org.gradle.api.artifacts.component.ProjectComponentIdentifier
 import org.gradle.api.artifacts.result.ResolvedArtifactResult
 import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
@@ -21,7 +38,6 @@ import org.gradle.api.tasks.UntrackedTask
 import org.gradle.internal.component.external.model.ModuleComponentArtifactIdentifier
 import slack.gradle.register
 
-// TODO what about java files? What does the kt_jvm_library rule do?
 /** A spec for a plain kotlin jvm project. */
 internal class JvmProjectSpec(builder: Builder) {
   /**
@@ -29,38 +45,38 @@ internal class JvmProjectSpec(builder: Builder) {
    * multiple targets.
    */
   val name: String = builder.name
+  // Deps
   val deps: List<Dep> = builder.deps
   val exportedDeps: List<Dep> = builder.exportedDeps
-  /** Source globs. */
+  val testDeps: List<Dep> = builder.testDeps + Dep.Target("${name}_lib")
+  // Source globs
   val srcGlobs: List<String> = builder.srcGlobs
+  val testSrcGlobs: List<String> = builder.testSrcGlobs
 
   override fun toString(): String {
-    val deps =
-      if (deps.isNotEmpty()) {
-        "deps = " +
-          deps.joinToString(separator = ",\n", prefix = "[", postfix = "]") { "        \"$it\"" } +
-          ","
-      } else {
-        ""
-      }
-    val exportedDeps =
-      if (exportedDeps.isNotEmpty()) {
-        "exportedDeps = " +
-          exportedDeps.joinToString(separator = ",\n", prefix = "[", postfix = "]") {
-            "        \"$it\""
-          } +
-          ","
-      } else {
-        ""
-      }
+    val compositeTestDeps = (deps + exportedDeps + testDeps).toSortedSet()
+    val deps = depsString("deps", deps)
+    val exportedDeps = depsString("exportedDeps", exportedDeps)
+    val testDeps = depsString("deps", compositeTestDeps)
 
     /*
-     load("@rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library")
+     load("@rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library", "kt_jvm_test")
 
      kt_jvm_library(
-         name = "ditto",
-         srcs = glob(["src/**/*.kt"]),
+         name = "ditto_lib",
+         srcs = glob(["src/main/**/*.kt"]),
+         visibility = ["//visibility:public"],
          deps = [
+             "@maven//:androidx_annotation_annotation",
+         ],
+     )
+
+     kt_jvm_library(
+         name = "ditto_test",
+         srcs = glob(["src/test/**/*.kt"]),
+         visibility = ["//visibility:private"],
+         deps = [
+             ":ditto_lib",
              "@maven//:androidx_annotation_annotation",
              "@maven//:org_jetbrains_kotlinx_kotlinx_coroutines_test",
              "@maven//:org_jetbrains_kotlin_kotlin_test",
@@ -71,14 +87,21 @@ internal class JvmProjectSpec(builder: Builder) {
     */
 
     return """
-      load("@rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library")
+      load("@rules_kotlin//kotlin:jvm.bzl", "kt_jvm_library", "kt_jvm_test")
 
       kt_jvm_library(
-          name = "$name",
+          name = "${name}_lib",
           srcs = glob(${srcGlobs.joinToString(separator = ",", prefix = "[", postfix = "]", transform = {"\"$it\""})}),
           visibility = ["//visibility:public"],
           $deps
           $exportedDeps
+      )
+
+      kt_jvm_test(
+          name = "${name}_test",
+          srcs = glob(${testSrcGlobs.joinToString(separator = ",", prefix = "[", postfix = "]", transform = {"\"$it\""})}),
+          visibility = ["//visibility:private"],
+          $testDeps
       )
     """
       .trimIndent()
@@ -89,10 +112,22 @@ internal class JvmProjectSpec(builder: Builder) {
     fs.write(path.resolve("BUILD.bazel")) { writeUtf8(toString()) }
   }
 
+  private fun depsString(name: String, deps: Collection<Dep>): String {
+    return if (deps.isNotEmpty()) {
+      "$name = " +
+        deps.joinToString(separator = ",\n", prefix = "[", postfix = "]") { "        \"$it\"" } +
+        ","
+    } else {
+      ""
+    }
+  }
+
   class Builder(val name: String) {
     val deps = mutableListOf<Dep>()
     val exportedDeps = mutableListOf<Dep>()
-    val srcGlobs = mutableListOf("src/**/*.kt")
+    val testDeps = mutableListOf<Dep>()
+    val srcGlobs = mutableListOf("src/main/**/*.kt", "src/main/**/*.java")
+    val testSrcGlobs = mutableListOf("src/test/**/*.kt", "src/test/**/*.java")
 
     fun addDep(dep: Dep) {
       deps.add(dep)
@@ -104,6 +139,10 @@ internal class JvmProjectSpec(builder: Builder) {
 
     fun addSrcGlob(glob: String) {
       srcGlobs.add(glob)
+    }
+
+    fun addTestSrcGlob(glob: String) {
+      testSrcGlobs.add(glob)
     }
 
     fun build(): JvmProjectSpec = JvmProjectSpec(this)
@@ -128,28 +167,45 @@ internal abstract class JvmProjectBazelTask : DefaultTask() {
 
   @TaskAction
   fun generate() {
-    // TODO map identifiers to lower_underscore case and convert to bazel deps
-    val depsIdentifiers = deps.mapToIdentifiers().get()
-    val exportedDepsIdentifiers = exportedDeps.mapToIdentifiers().get()
+    val depsIdentifiers = deps.mapDeps()
+    val exportedDepsIdentifiers = exportedDeps.mapDeps()
+
+    JvmProjectSpec.Builder(name.get())
+      .apply {
+        depsIdentifiers.forEach { addDep(it) }
+        exportedDepsIdentifiers.forEach { addExportedDep(it) }
+      }
+      .build()
+      .writeTo(projectDir.get().asFile.toOkioPath())
   }
 
-  // TODO just return converted Bazel deps instead?
-  private fun SetProperty<ResolvedArtifactResult>.mapToIdentifiers():
-    Provider<Map<String, String>> {
+  private fun SetProperty<ResolvedArtifactResult>.mapDeps(): SortedSet<Dep> {
     return map { result ->
-      result
-        .asSequence()
-        .map { it.id }
-        .filterIsInstance<ModuleComponentArtifactIdentifier>()
-        .associate { component ->
-          val componentId = component.componentIdentifier
-          val identifier = "${componentId.group}:${componentId.module}"
-          identifier to componentId.version
-        }
-    }
+        result
+          .asSequence()
+          .map { it.id }
+          .map { component ->
+            when (component) {
+              is ModuleComponentArtifactIdentifier -> {
+                val componentId = component.componentIdentifier
+                val identifier = "${componentId.group}:${componentId.module}"
+
+                // Map to lower underscore format for maven sourcing
+                val target = identifier.replace(".", "_").replace(":", "_").replace("-", "_")
+                Dep.Remote(source = "maven", path = "", target = target)
+              }
+              is ProjectComponentIdentifier -> {
+                // Map to "path/to/local/dependency1" format
+                Dep.Local(component.projectPath.removePrefix(":").replace(":", "/"))
+              }
+              else -> error("Unknown component type: $component")
+            }
+          }
+      }
+      .get()
+      .toSortedSet()
   }
 
-  // TODO need to update this to include project deps too
   protected fun resolvedDependenciesFrom(
     configuration: Configuration
   ): Provider<Set<ResolvedArtifactResult>> {
@@ -159,9 +215,6 @@ internal abstract class JvmProjectBazelTask : DefaultTask() {
           attribute(AndroidArtifacts.ARTIFACT_TYPE, AndroidArtifacts.ArtifactType.AAR_OR_JAR.type)
         }
         lenient(true)
-        // Only resolve external dependencies! Without this, all project dependencies will get
-        // _compiled_.
-        componentFilter { id -> id is ModuleComponentIdentifier }
       }
       .artifacts
       .resolvedArtifacts
