d80c8c9b35ef75897ca4c826074825af7b691625
Zac Sweers <pandanomic@gmail.com>
2024-02-28 17:30:20 -0500

    Add a target dep type + improve comparing

--- slack-plugin/src/main/kotlin/slack/gradle/bazel/Dep.kt
+++ slack-plugin/src/main/kotlin/slack/gradle/bazel/Dep.kt
@@ -1,7 +1,24 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package slack.gradle.bazel
 
 /** A Bazel dependency. */
-internal sealed interface Dep {
+internal sealed interface Dep : Comparable<Dep> {
+  override fun compareTo(other: Dep) = toString().compareTo(other.toString())
+
   /**
    * A remote dependency, e.g. `@maven//path:target`.
    *
@@ -26,5 +43,14 @@ internal sealed interface Dep {
     }
   }
 
-  companion object COMPARATOR : Comparator<Dep> by compareBy(Dep::toString)
+  /**
+   * A target dependency, e.g. `:main_lib`.
+   *
+   * @param name the name to depend on, e.g. `main_lib`
+   */
+  data class Target(val name: String) : Dep {
+    override fun toString(): String {
+      return ":$name"
+    }
+  }
 }
