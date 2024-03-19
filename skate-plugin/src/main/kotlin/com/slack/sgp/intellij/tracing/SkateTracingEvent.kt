bb3d87e7db464f352b42866e562d72c27d85914d
Linh Pham <lpham@slack-corp.com>
2024-03-19 14:45:57 -0700

    Trace Gradle Sync with Skate (#785)
    Add hooks to trace Gradle sync time with Skate
    
    Sample data:
    https://analytics.tinyspeck.com/v3/explore/1000000001280941
    
    <!--
      ⬆ Put your description above this! ⬆
    
      Please be descriptive and detailed.
      
    Please read our [Contributing
    Guidelines](https://github.com/tinyspeck/slack-gradle-plugin/blob/main/.github/CONTRIBUTING.md)
    and [Code of Conduct](https://slackhq.github.io/code-of-conduct).
    
    Don't worry about deleting this, it's not visible in the PR!
    -->

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/tracing/SkateTracingEvent.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/tracing/SkateTracingEvent.kt
@@ -44,4 +44,10 @@ sealed interface SkateTracingEvent {
     SCANNING_TYPE,
     INDEXING_COMPLETED,
   }
+
+  enum class GradleSync : SkateTracingEvent {
+    GRADLE_SYNC_STARTED,
+    GRADLE_SYNC_SUCCEDDED,
+    GRADLE_SYNC_SKIPPED,
+  }
 }
