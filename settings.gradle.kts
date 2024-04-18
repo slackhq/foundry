3d9032c143f4cbc7f5045c1659c6d31f54a9e73e
Zac Sweers <pandanomic@gmail.com>
2024-04-18 00:55:58 -0400

    Migrate to develocity APIs for scans and tags
    Gradle offers a helpful adapter API to bridge across both the old and new enterprise/develocity plugins. This also replaces our ScanApi

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -78,7 +78,7 @@ dependencyResolutionManagement {
       forRepository(::gradlePluginPortal)
       filter {
         includeModule("com.github.ben-manes", "gradle-versions-plugin")
-        includeModule("com.gradle", "gradle-enterprise-gradle-plugin")
+        includeModule("com.gradle", "develocity-gradle-plugin")
         includeModule("org.gradle", "test-retry-gradle-plugin")
         includeModule("gradle.plugin.org.gradle.android", "android-cache-fix-gradle-plugin")
         includeModule("net.ltgt.gradle", "gradle-errorprone-plugin")
@@ -144,8 +144,8 @@ pluginManagement {
           "com.github.ben-manes.versions",
           "com.github.ben-manes.versions.gradle.plugin",
         )
-        includeModule("com.gradle", "gradle-enterprise-gradle-plugin")
-        includeModule("com.gradle.enterprise", "com.gradle.enterprise.gradle.plugin")
+        includeModule("com.gradle", "develocity-gradle-plugin")
+        includeModule("com.gradle.develocity", "com.gradle.develocity.gradle.plugin")
         includeModule("com.diffplug.spotless", "com.diffplug.spotless.gradle.plugin")
         includeModule("io.gitlab.arturbosch.detekt", "io.gitlab.arturbosch.detekt.gradle.plugin")
         includeModule("org.gradle.kotlin.kotlin-dsl", "org.gradle.kotlin.kotlin-dsl.gradle.plugin")
@@ -163,18 +163,17 @@ pluginManagement {
       }
     }
   }
-  plugins { id("com.gradle.enterprise") version "3.14.1" }
 }
 
-plugins { id("com.gradle.enterprise") }
+plugins { id("com.gradle.develocity") version "3.17.2" }
 
 val VERSION_NAME: String by extra.properties
 
-gradleEnterprise {
+develocity {
   buildScan {
-    publishAlways()
-    termsOfServiceUrl = "https://gradle.com/terms-of-service"
-    termsOfServiceAgree = "yes"
+    publishing { onlyIf { true } }
+    termsOfUseUrl.set("https://gradle.com/terms-of-service")
+    termsOfUseAgree.set("yes")
 
     tag(if (System.getenv("CI").isNullOrBlank()) "Local" else "CI")
     tag(VERSION_NAME)
