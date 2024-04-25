f81ca3c559984e7ee3151bfe593f47afa0807aa0
Zac Sweers <pandanomic@gmail.com>
2024-04-25 14:24:57 -0400

    Spotless

--- skate-plugin/compose-playground/src/jvmMain/kotlin/slack/tooling/compose/playground/MarkdownPlayground.kt
+++ skate-plugin/compose-playground/src/jvmMain/kotlin/slack/tooling/compose/playground/MarkdownPlayground.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package slack.tooling.compose.playground
 
 import androidx.compose.foundation.background
@@ -31,9 +46,9 @@ fun main() = singleWindowApplication {
 private const val MARKDOWN =
   """
   # Markdown Playground
-  
+
   ---
-  
+
   # This is an H1
 
   ## This is an H2
@@ -45,40 +60,40 @@ private const val MARKDOWN =
   ##### This is an H5
 
   ###### This is an H6
-  
+
   This is a paragraph with some *italic* and **bold** text.
-  
+
   This is a paragraph with some `inline code`.
-  
+
   This is a paragraph with a [link](https://www.jetbrains.com/).
-  
+
   This is a code block:
   ```kotlin
   fun main() {
     println("Hello, world!")
   }
   ```
-  
+
   > This is a block quote.
-  
+
   This is a divider
-  
+
   ---
-  
+
   The above was supposed to be a divider.
-  
+
   ~~This is strikethrough~~
-  
+
   This is an ordered list:
   1. Item 1
   2. Item 2
   3. Item 3
-  
+
   This is an unordered list with dashes:
   - Item 1
   - Item 2
   - Item 3
-  
+
   This is an unordered list with asterisks:
   * Item 1
   * Item 2
