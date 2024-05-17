7a3d29e469c881925576db8bb8bc42f5ea29b4fe
Julio Perez <jperez@slack-corp.com>
2024-05-17 11:51:09 -0700

    Support for scalars and objects
    This now supports both types of "path". I'll look at splitting up the code into separate files for clarity. Not 100% sure this is the best way to do this, but looking forward to comments during review.

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/codeowners/model/CodeOwner.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/codeowners/model/CodeOwner.kt
@@ -15,46 +15,70 @@
  */
 package com.slack.sgp.intellij.codeowners.model
 
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
+import com.charleskorn.kaml.YamlInput
+import com.charleskorn.kaml.YamlMap
+import com.charleskorn.kaml.YamlScalar
+import kotlinx.serialization.*
 import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.*
 import kotlinx.serialization.encoding.*
 
-object PathDeserializer : KSerializer<Path> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("Path") {
-        element("path", String.serializer().descriptor)
-        element("notify", Boolean.serializer().descriptor)
+object StringOrObjectSerializer : KSerializer<Path> {
+    private val stringDescriptor = String.serializer().descriptor
+    private val objectDescriptor = buildClassSerialDescriptor("Path") {
+        element<String>("path")
+        element<Boolean>("notify", isOptional = true)
     }
 
-    @OptIn(ExperimentalSerializationApi::class)
+    @OptIn(ExperimentalSerializationApi::class, InternalSerializationApi::class)
+    override val descriptor: SerialDescriptor = buildSerialDescriptor(
+        StringOrObjectSerializer::class.java.name,
+        SerialKind.CONTEXTUAL
+    ) {
+        element("object", objectDescriptor)
+        element("string", stringDescriptor)
+    }
+
     override fun deserialize(decoder: Decoder): Path {
-        return decoder.decodeStructure(descriptor) {
-            var path = ""
-            var notify = true
+        val tree = decoder.beginStructure(descriptor) as YamlInput
+        val result = when (tree.node) {
+            is YamlScalar -> beginAndDecodeString(tree)
+            is YamlMap -> beginAndDecodeObject(tree)
+            else -> error("Unexpected type, path should be string or map")
+        }
+        return result
+    }
+
+    private fun beginAndDecodeString(decoder: YamlInput): Path {
+        val input = decoder.beginStructure(stringDescriptor) as YamlInput
+        val path = input.decodeString().also { decoder.endStructure(descriptor) }
+        return Path(path, true)
+    }
+
+    private fun beginAndDecodeObject(decoder: YamlInput): Path {
+        var path = ""
+        var notify = true
+        decoder.decodeStructure(objectDescriptor) {
             while (true) {
-                when (val index = decodeElementIndex(descriptor)) {
-                    0 -> path = decodeStringElement(descriptor, 0)
-                    1 -> notify = decodeBooleanElement(descriptor, 1)
+                when (val index = decodeElementIndex(objectDescriptor)) {
+                    0 -> path = decodeStringElement(objectDescriptor, 0)
+                    1 -> notify = decodeBooleanElement(objectDescriptor, 1)
                     CompositeDecoder.DECODE_DONE -> break
                     else -> error("Unexpected index: $index")
                 }
             }
-            Path(path, notify)
         }
+        return Path(path, notify)
     }
 
-    @OptIn(ExperimentalSerializationApi::class)
     override fun serialize(encoder: Encoder, value: Path) {
-        encoder.encodeStructure(descriptor) {
-            encodeStringElement(descriptor, 0, value.path)
-        }
+        TODO("Not yet implemented")
     }
+
 }
 
-@Serializable(with = PathDeserializer::class)
+@Serializable
 data class Path(val path: String, val notify: Boolean?)
 
 @Serializable
-data class CodeOwner(val name: String, val paths: List<@Serializable(with = PathDeserializer::class)Path>)
+data class CodeOwner(val name: String, val paths: List<@Serializable(with = StringOrObjectSerializer::class) Path>)
