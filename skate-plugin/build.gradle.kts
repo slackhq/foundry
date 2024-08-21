52742fb4285753f5503f0332a073464f8417c7f5
kateliu20 <katherineliu@slack-corp.com>
2024-08-21 17:02:52 -0400

    Add conversation text field, send and clear chat buttons

--- skate-plugin/build.gradle.kts
+++ skate-plugin/build.gradle.kts
@@ -128,11 +128,16 @@ dependencies {
   implementation(projects.skatePlugin.projectGen)
   implementation(projects.tracing)
 
+  implementation(compose.animation)
   implementation(compose.desktop.common)
-  implementation(compose.desktop.currentOs)
+  implementation(compose.desktop.linux_arm64)
+  implementation(compose.desktop.linux_x64)
+  implementation(compose.desktop.macos_arm64)
+  implementation(compose.desktop.macos_x64)
+  implementation(compose.desktop.windows_x64)
   implementation(compose.foundation)
+  implementation(compose.material)
   implementation(compose.material3)
-  implementation(compose.runtime)
   implementation(compose.ui)
 
   testImplementation(libs.junit)
--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatWindowCompose.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatWindowCompose.kt
@@ -1,19 +1,72 @@
 package com.slack.sgp.intellij.aibot
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun ChatWindowCompose(){
-    Column(modifier = Modifier.padding(all = 8.dp)){
-        Text(
-            text = "Welcome to the DevXP AI Chat Bot!",
-            color = Color.White
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom
+    ){
+        ConversationField()
+    }
+}
+
+@Composable
+fun ConversationField(){
+    var text by remember { mutableStateOf("") }
+    Row(){
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Start your conversation") },
+            modifier = Modifier
+                .weight(1f)
         )
+        Column(){
+            Box(
+                modifier = Modifier
+                    .clickable{
+                        text = ""
+                    }
+                    .defaultMinSize(minWidth = 56.dp)
+                    .padding(8.dp)
+            ){
+                Text(
+                    "Send",
+                    color = MaterialTheme.colorScheme.surface
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .clickable{
+                        text = ""
+                    }
+                    .defaultMinSize(minWidth = 56.dp)
+                    .padding(8.dp)
+            ){
+                Text(
+                    "Clear Chat",
+                    color = MaterialTheme.colorScheme.surface
+                )
+            }
+        }
     }
 }
