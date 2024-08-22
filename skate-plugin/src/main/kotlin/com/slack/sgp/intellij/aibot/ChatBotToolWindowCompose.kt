49e39d0a14af2c7025f9dfade57788e5a5657a01
kateliu20 <katherineliu@slack-corp.com>
2024-08-22 12:06:10 -0400

    Run spotless, revert sortDependencies version ref

--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatBotToolWindowCompose.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatBotToolWindowCompose.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.slack.sgp.intellij.aibot
 
 import androidx.compose.ui.awt.ComposePanel
@@ -7,17 +22,13 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
 class ChatBotToolWindowCompose : ToolWindowFactory {
-    override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val contentFactory = ContentFactory.getInstance()
-        val content = contentFactory.createContent(createComposePanel(), "", false)
-        toolWindow.contentManager.addContent(content)
-    }
+  override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+    val contentFactory = ContentFactory.getInstance()
+    val content = contentFactory.createContent(createComposePanel(), "", false)
+    toolWindow.contentManager.addContent(content)
+  }
 
-    private fun createComposePanel(): ComposePanel {
-        return ComposePanel().apply {
-            setContent {
-                ChatWindowCompose()
-            }
-        }
-    }
+  private fun createComposePanel(): ComposePanel {
+    return ComposePanel().apply { setContent { ChatWindowCompose() } }
+  }
 }
--- skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatWindowCompose.kt
+++ skate-plugin/src/main/kotlin/com/slack/sgp/intellij/aibot/ChatWindowCompose.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.slack.sgp.intellij.aibot
 
 import androidx.compose.foundation.clickable
@@ -17,56 +32,38 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun ChatWindowCompose(){
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom
-    ){
-        ConversationField()
-    }
+fun ChatWindowCompose(modifier: Modifier = Modifier) {
+  Column(modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Bottom) {
+    ConversationField()
+  }
 }
 
 @Composable
-fun ConversationField(){
-    var text by remember { mutableStateOf("") }
-    Row(){
-        TextField(
-            value = text,
-            onValueChange = { text = it },
-            label = { Text("Start your conversation") },
-            modifier = Modifier
-                .weight(1f)
-        )
-        Column(){
-            Box(
-                modifier = Modifier
-                    .clickable{
-                        text = ""
-                    }
-                    .defaultMinSize(minWidth = 56.dp)
-                    .padding(8.dp)
-            ){
-                Text(
-                    "Send",
-                    color = MaterialTheme.colorScheme.surface
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .clickable{
-                        text = ""
-                    }
-                    .defaultMinSize(minWidth = 56.dp)
-                    .padding(8.dp)
-            ){
-                Text(
-                    "Clear Chat",
-                    color = MaterialTheme.colorScheme.surface
-                )
-            }
-        }
+fun ConversationField(modifier: Modifier = Modifier) {
+  var textValue by remember { mutableStateOf(TextFieldValue("")) }
+  Row(modifier) {
+    TextField(
+      value = textValue,
+      onValueChange = { newText -> textValue = newText },
+      label = { Text("Start your conversation") },
+      modifier = Modifier.weight(1f),
+    )
+    Column() {
+      Box(modifier = Modifier.clickable {}.defaultMinSize(minWidth = 56.dp).padding(8.dp)) {
+        Text("Send", color = MaterialTheme.colorScheme.surface)
+      }
+      Box(
+        modifier =
+          Modifier.clickable { textValue = TextFieldValue("") }
+            .defaultMinSize(minWidth = 56.dp)
+            .padding(8.dp)
+      ) {
+        Text("Clear Chat", color = MaterialTheme.colorScheme.surface)
+      }
     }
+  }
 }
