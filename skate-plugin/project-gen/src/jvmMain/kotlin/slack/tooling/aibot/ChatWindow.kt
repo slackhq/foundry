1ac1eac1ac7937b4f3de1017c8d5957fcd3e5ba5
kateliu20 <katherineliu@slack-corp.com>
2024-09-10 10:58:19 -0400

    Rename ChatWindowUi back to ChatWindow

--- skate-plugin/project-gen/src/jvmMain/kotlin/slack/tooling/aibot/ChatWindow.kt
+++ skate-plugin/project-gen/src/jvmMain/kotlin/slack/tooling/aibot/ChatWindow.kt
@@ -59,9 +59,9 @@ import org.jetbrains.jewel.ui.component.IconButton
 import org.jetbrains.jewel.ui.component.Text
 import org.jetbrains.jewel.ui.component.TextArea
 
-object ChatWindowUi {
+object ChatWindow {
   @Composable
-  fun ChatWindow(state: ChatScreen.State, modifier: Modifier = Modifier) {
+  fun ChatWindowUi(state: ChatScreen.State, modifier: Modifier = Modifier) {
     Column(modifier = modifier.fillMaxSize().background(JewelTheme.globalColors.paneBackground)) {
       LazyColumn(modifier = Modifier.weight(1f), reverseLayout = true) {
         items(state.messages.reversed()) { message ->
