fdfee7e741f6918ec35e7e982ccc1fc3477c315e
Zac Sweers <pandanomic@gmail.com>
2024-10-28 14:42:50 -0400

    Spotless

--- platforms/gradle/foundry-gradle-plugin/src/main/kotlin/foundry/gradle/model/ModuleTopographyTask.kt
+++ platforms/gradle/foundry-gradle-plugin/src/main/kotlin/foundry/gradle/model/ModuleTopographyTask.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2024 Slack Technologies, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package foundry.gradle.model
 
 import com.squareup.moshi.JsonClass
@@ -168,7 +183,7 @@ public abstract class ModuleTopographyTask : DefaultTask() {
       logger.error(
         """
           ${featuresToRemove.joinToString("\n"){ "- ${it.removalMessage}" }}
-          
+
           Validation errors written to ${featuresToRemoveOutputFile.asFile.get().absolutePath}
         """
           .trimIndent()
@@ -188,9 +203,9 @@ public abstract class ModuleTopographyTask : DefaultTask() {
       error(
         """
           Validation failed for the following features:
-          
+
           ${featuresToValidate.joinToString("\n", transform = ModuleFeature::removalMessage)}
-          
+
           Full list written to ${featuresToRemoveOutputFile.asFile.get().absolutePath}
         """
           .trimIndent()
