b7559186acad1934af150af065571ce9ca27e46c
Kate Liu <katherineliu@slack-corp.com>
2025-03-31 11:25:14 -0400

    Add initial test class

---
+++ platforms/intellij/skate/src/test/kotlin/foundry/intellij/skate/projectgen/ProjectGenWindowTest.kt
@@ -0,0 +1,7 @@
+package foundry.intellij.skate.projectgen
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixture4TestCase
+
+class ProjectGenWindowTest : LightPlatformCodeInsightFixture4TestCase() {
+
+}
