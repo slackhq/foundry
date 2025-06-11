66c9a84c4f9543cb44b1f2bcc0b3bc093372a4df
Kate Liu <katherineliu@slack-corp.com>
2025-06-11 13:23:41 -0400

    Clean up aibot code since it's unused

--- platforms/intellij/compose/src/jvmMain/kotlin/foundry/intellij/compose/util/PainterResource.kt
+++ platforms/intellij/compose/src/jvmMain/kotlin/foundry/intellij/compose/util/PainterResource.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package foundry.intellij.compose.aibot
+package foundry.intellij.compose.util
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
