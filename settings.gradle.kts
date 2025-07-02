49e2696362d698371fefdd5e097d24aa8faa2a4b
Zac Sweers <pandanomic@gmail.com>
2025-07-02 00:56:07 -0400

    Fix classpath

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -62,6 +62,9 @@ buildscript {
   dependencies {
     // Force a newer version of okio, otherwise intellijPlatform and wire conflict
     classpath("com.squareup.okio:okio:3.14.0")
+    // For some reason we need to enforce this on the classpath early
+    // See https://github.com/tinyspeck/slack-android-ng/pull/107076
+    classpath("com.squareup:kotlinpoet:2.2.0")
   }
 }
 
