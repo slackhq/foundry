27f74c5d52cb5a5831271a1a6fced2f96447011a
Zac Sweers <pandanomic@gmail.com>
2025-10-02 14:21:02 -0400

    Baselines

--- platforms/gradle/foundry-gradle-plugin/src/main/kotlin/foundry/gradle/StandardProjectConfigurations.kt
+++ platforms/gradle/foundry-gradle-plugin/src/main/kotlin/foundry/gradle/StandardProjectConfigurations.kt
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-@file:Suppress("UnstableApiUsage")
-
 package foundry.gradle
 
 import app.cash.sqldelight.gradle.SqlDelightExtension
