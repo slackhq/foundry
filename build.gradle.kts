8ffa24ffb8d2be0d41c99953286c9c2a0846c9a2
Zac Sweers <pandanomic@gmail.com>
2025-10-02 16:16:21 -0400

    Suppress deprecated IJ API + enable -Werror on buildscripts (#1562)
    This property makes all kotlin buildscript deprecation warnings fail the
    build so we don't miss 'em in the future
    https://docs.gradle.org/current/userguide/kotlin_dsl.html#sec:compilation_warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -345,7 +345,9 @@ subprojects {
       }
       project.dependencies {
         configure<IntelliJPlatformDependenciesExtension> {
-          intellijIdeaCommunity(libs.versions.intellij.version)
+          // Suppressed deprecation as we can't actually move to intellijIdea() until 2025.3
+          // Bad use of the deprecated annotation
+          @Suppress("DEPRECATION") intellijIdeaCommunity(libs.versions.intellij.version)
         }
       }
 
